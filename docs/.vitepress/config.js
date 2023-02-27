29028f44bbeefd3605912e402eeb6bb6538ba602
hojin <jhj377@gmail.com>
2023-02-28 02:27:34 +0900

    del base

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -1,11 +1,11 @@
 export default {
   title: 'TLDR',
   description: 'More Simply',
-  base: '/static-site/',
+  // base: '/static-site/',
   cleanUrls: true,
   themeConfig: {
     footer: {
-      message: '<a href="/static-site/clubhouse-privacy-policy/">Privacy Policy</a> | <a href="/static-site/clubhouse-term-conditions/">Term & Conditions</a>',
+      message: '<a href="/clubhouse-privacy-policy/">Privacy Policy</a> | <a href="/clubhouse-term-conditions/">Term & Conditions</a>',
       copyright: 'Copyright © 2023-present <a href="/index">TLDR</a>'
     }
   }
