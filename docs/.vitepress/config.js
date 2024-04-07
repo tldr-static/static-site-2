e61f4ff4819bb3028b8c059eef56a5d4842188bf
hojin <jhj377@gmail.com>
2024-04-08 00:52:10 +0900

    Update config.js

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -4,9 +4,5 @@ export default {
   // base: '/static-site/',
   cleanUrls: true,
   themeConfig: {
-    footer: {
-      message: '<a href="/clubhouse-privacy-policy/">Privacy Policy</a> | <a href="/clubhouse-term-conditions/">Term & Conditions</a>',
-      copyright: 'Copyright © 2023-present <a href="/index">TLDR</a>'
-    }
   }
 }
